beac8916c75e74508a59a61a7ea06762bc63ddfd
Adrian Letchford <me@dradrian.com>
2023-05-12 12:33:34 +0100

    Switch to QuestDB

--- trader/trader.py
+++ trader/trader.py
@@ -2,15 +2,18 @@ from time import sleep
 import numpy as np
 import structlog
 import logging
+import os
 
+LOG_FILE_NAME = os.environ['LOG_FILE_NAME']
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 console_handler = logging.StreamHandler()
 logger.addHandler(console_handler)
-file_handler = logging.FileHandler('/var/log/trader/trader.logs')
+file_handler = logging.FileHandler(LOG_FILE_NAME)
 logger.addHandler(file_handler)
 
+# Configure the structured logger
 structlog.configure_once(
     processors=[
         structlog.stdlib.add_log_level,
@@ -19,14 +22,15 @@ structlog.configure_once(
     ],
 )
 
+# Wrap the root logger with the structured logger
 json_logger = structlog.wrap_logger(logger)
 
 price = 100
 
-json_logger.info(event="startup")
-
 while True:
-    sleep(0.5)
-
     price = price * (1 + np.random.randn() * 0.01)
-    json_logger.info(event="price", price=price)
+
+    # Log price in JSON
+    json_logger.info(event="price_log", price=price)
+
+    sleep(0.5)
